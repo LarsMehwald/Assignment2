d3129d209d8e2803d735fce358d72b055f43bdef
LarsMehwald <lars.mehwald@posteo.de>
2015-10-21 17:21:06 +0200

    Spread command

--- FirstDoFile.R
+++ FirstDoFile.R
@@ -5,6 +5,7 @@
 ########################
 
 library("rio")
+library("dplyr")
 search()
 
 URL_PKS_faelle <- "http://www.bka.de/SharedDocs/Downloads/DE/Publikationen/PolizeilicheKriminalstatistik/2014/Zeitreihen/Faelle/tb01__FaelleGrundtabelleAb1987__csv,templateId=raw,property=publicationFile.csv/tb01__FaelleGrundtabelleAb1987__csv.csv"
@@ -32,7 +33,7 @@ VarName1
 names(PKS_Kreise)[1] <- PKS_Kreise[1,1] # Problem persists
 
 # New apptempt to rename, this time manually 
-PKS_Kreise[1,]
+PKS_Kreise_13_14[1,]
 NameofVariables <- c("Schlüsselzahl der Tat", "Straftat", "Gemeindeschlüssel", "Stadt-/Landkreis", "erfasste Fälle", "HZ", "Versuche - Anzahl", "Versuche - Anteil in %", "mit Schusswaffe gedroht", "mit Schusswaffe geschossen", "aufgeklärte Fälle", "Aufklärungsquote", "Tatverdächtige insgesamt", "Tatverdächtige - männlich", "Tatverdächtige - weiblich", "Nichtdeutsche Tatverdächtige - Anzahl", "Nichtdeutsche Tatverdächtige - Anteil in %")
 names(PKS_Kreise) <- NameofVariables 
 rm(NameofVariables)
@@ -51,12 +52,11 @@ PKS_Kreise <- PKS_Kreise[c(2,1,3)]
 library("tidyr")
 search()
 
-# PKS_Kreise_gather <- gather(PKS_Kreise, "Straftat", "erfasste Fälle", "Gemeindeschlüssel")
 # Do I need gather or spread? 
 # Do I need to reformat the data? 
 
 # Setting the class type to numeric: error
-class(PKS_Kreise$"Gemeindeschlüssel") <- numeric # Why is this not working? 
+# class(PKS_Kreise$"Gemeindeschlüssel") <- numeric # Why is this not working? 
 
 ########################
 # Kreise 2013 2014
@@ -78,3 +78,13 @@ PKS_Kreise_13_14 <- PKS_Kreise_13_14[-c(1),]
 
 # Rearranging data
 PKS_Kreise_13_14 <- PKS_Kreise_13_14[c(2,1,4,3)]
+
+# Spread data
+
+PKS_Kreise_13_14[1,]
+
+PKS_Kreise_13 <- PKS_Kreise_13_14[,c(1,2,3)]
+PKS_Kreise_14 <- PKS_Kreise_13_14[,c(1,2,4)]
+
+PKS_Kreise_13_spread <- spread(PKS_Kreise_13, "Straftat", "2013 - erfasste Fälle")
+PKS_Kreise_14_spread <- spread(PKS_Kreise_14, "Straftat", "2014 - erfasste Fälle")
